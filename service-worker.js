af6ffaf2c49a554a4d7a6833a8fe5d8329dadf6a
abbasbalkhi2010 <abbas.balkhi2010@gmail.com>
2025-03-03 11:32:36 +0330

    Initial commit of Export to JPG PWA

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'export-to-jpg-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return cached response if available
+        }
+        return fetch(event.request); // Fetch from network if not cached
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
